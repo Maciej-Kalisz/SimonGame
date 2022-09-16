ca4e970c3f51474fa5535b04998e0a50ab634366
Maciej-Kalisz <hello@maciejkalisz.co.uk>
2022-09-16 20:08:58 +0100

    Adding basic click functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
-alert("Hello, world!");
+jq.$("#red").click(function (e) { 
+    e.preventDefault();
+    
+    // Add "pressed" class when button is pressed
+    $("#red").addClass("pressed");
+
+    // Remove "pressed" class after 0.5s (500 milliseconds)
+    setTimeout(function () {
+        $("#red").removeClass("pressed");
+    }, 500);
+});
+
+jq.$("#green").click(function (e) { 
+    e.preventDefault();
+    
+    // Add "pressed" class when button is pressed
+    $("#green").addClass("pressed");
+
+    // Remove "pressed" class after 0.5s (500 milliseconds)
+    setTimeout(function () {
+        $("#green").removeClass("pressed");
+    })
+});
+
+jq.$("#blue").click(function (e) { 
+    e.preventDefault();
+    
+     // Add "pressed" class when button is pressed
+   $("#blue").addClass("pressed");
+
+    // Remove "pressed" class after 0.5s (500 milliseconds)
+    setTimeout(function () {
+        $("#blue").removeClass("pressed");
+    })
+});
+
+jq.$("#yellow").click(function (e) { 
+    e.preventDefault();
+    
+    // Add "pressed" class when button is pressed
+    $("#yellow").addClass("pressed");
+
+    // Remove "pressed" class after 0.5s (500 milliseconds)
+    setTimeout(function () {
+        $("#yellow").removeClass("pressed");
+    })
+});
